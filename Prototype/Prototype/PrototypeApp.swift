83260465ff6df4b412b6036e02a0a64ce291ef8c
Lizik Gábor Levente <lizik.gabor@microsec.hu>
2024-01-15 16:11:53 +0100

    Add Empty Project to prototype

---
+++ Prototype/Prototype/PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Lizik Gábor on 15/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
