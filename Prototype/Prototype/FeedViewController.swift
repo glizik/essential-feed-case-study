bec8d371d96b9d2be0fc58192332e261adad8882
Gabor Lizik <glizik@gmail.com>
2024-02-11 13:22:05 +0100

    Add Prototype

---
+++ Prototype/Prototype/FeedViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FeedViewController.swift
+//  Prototype
+//
+//  Created by Lizik Gábor on 15/01/2024.
+//
+
+import UIKit
+
+struct FeedImageViewModel {
+    let description: String?
+    let location: String?
+    let imageName: String
+}
+
+final class FeedViewController: UITableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+    }
+}
