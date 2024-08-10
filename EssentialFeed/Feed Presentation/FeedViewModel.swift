0a3644689638f6d4c12650f501394eb63d2bb83e
Gabor Lizik <glizik@gmail.com>
2024-08-10 16:32:30 +0200

    move `FeedPresenter` to separate files

---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Gabor L Lizik on 10/08/2024.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
