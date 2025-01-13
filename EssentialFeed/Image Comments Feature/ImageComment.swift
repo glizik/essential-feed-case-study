06cf3a5c13ef0c8aedc3300a1baa800126440620
Gabor Lizik <glizik@gmail.com>
2025-01-13 09:55:29 +0100

    Add ImageComment data model

---
+++ EssentialFeed/Image Comments Feature/ImageComment.swift
@@ -0,0 +1,22 @@
+//
+//  ImageComment.swift
+//  EssentialFeed
+//
+//  Created by Gabor L Lizik on 2025. 01. 13..
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
