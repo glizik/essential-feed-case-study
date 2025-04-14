225c8b2dbb10b9576623af30187d4cb5cb0f172c
Lizik Gábor Levente <lizik.gabor@microsec.hu>
2025-04-14 15:15:31 +0200

    Implement Image Comments UI, Add default implementation for optional methods in the CellController protocol

---
+++ EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeed
+//
+//  Created by Lizik Gábor on 2025. 04. 14..
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
